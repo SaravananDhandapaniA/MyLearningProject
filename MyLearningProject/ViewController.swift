9a1c821486007e5fbeef50652c0a18d6b05bb074
Plantd-dev <plantd.dev12@gmail.com>
2022-05-11 13:13:59 +0530

    Initial Commit

---
+++ MyLearningProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLearningProject
+//
+//  Created by Tringapps on 11/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
